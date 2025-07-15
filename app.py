60a4ad090589aded7b0f635b8376e16bd46a513f
Nuria-lab <nuriainestorreso@gmail.com>
2025-07-15 17:09:56 -0300

    arreglar puertos de escucha

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def index():
     return render_template("index.html", movimientos=df, saldo=saldo)
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True)
